56698e9baa62ab7e914276accdc39ac18eca531c
mdzzaman01 <77083412+mdzzaman01@users.noreply.github.com>
2021-01-20 16:25:11 -0500

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,65 @@
+  
+    let firstName = 'md';
+    console.log(firstName);
+
+    const lastName = 'zzaman';
+    console.log(lastName);
+
+    const city = "New York";
+    console.log(city);
+
+    const borough = "Queens";
+    console.log(borough)
+
+    const area ='Briarwood';
+    console.log(area);
+
+    let population = '12345';
+    console.log(population);
+
+    /*variable */
+    
+    let lucasMass = 78;
+    let lucasHeight = 1.69;   
+    console.log(lucasMass + ' ' + lucasHeight); 
+
+    console.log(lucasMass / lucasHeight ** 2 = lucasMass / (lucasHeight * lucasHeight));
+    
+    let johnMass = 92;
+    let johnHeight = 1.95;
+    console.log(johnMass + ' ' + johnHeight);
+
+    console.log(johnMass / johnHeight ** 2 = johnMass / (johnHeight * johnHeight));
+
+    /*BMI*/ 
+    
+    
+
+
+    
+const firstName = "Lucas";
+const BMI = "";
+const year = 2021;
+
+console.log(firstName + 's' + BMI + ' ' + year + 'is' + age + 'old');
+const johnInfo = `${firstName}'s ${BMI} ,  ${year}  is ${age} old`;
+console.log(johInfo);
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
